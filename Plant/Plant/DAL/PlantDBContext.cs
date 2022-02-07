be9a247a553bff75c0bc9a9a500443688fbaa14a
Prit17 <patilpritishivaji@gmail.com>
2022-02-07 14:07:02 +0530

    Model
    Done

--- Plant/Plant/DAL/PlantDBContext.cs
+++ Plant/Plant/DAL/PlantDBContext.cs
@@ -24,10 +24,10 @@ namespace Plant.DAL
 
         //SILClassificationMaster
         public DbSet<SILClassificationMaster> SilClassificationMaster { get; set; }
-        public List<RiskMatrixMaster> RiskMatrixMaster { get; set; }
-        public List<InitiatingCausesMaster> InitiatingCausesMaster { get; set; }
-        public List<ConditionalModifiersMaster> ConditionalModifiersMaster { get; set; }
-        public List<IPLMaster> IplMaster { get; set; }
+        public DbSet<RiskMatrixMaster> RiskMatrixMaster { get; set; }
+        public DbSet<InitiatingCausesMaster> InitiatingCausesMaster { get; set; }
+        //  public DbSet<ConditionalModifiersMaster> ConditionalModifiersMaster { get; set; }
+        public DbSet<IPLMaster> IplMaster { get; set; }
 
         //SILClassification
         public DbSet<SILClassification> SilClassification { get; set; }
@@ -107,6 +107,9 @@ namespace Plant.DAL
                 .WithMany(r => r.MSS)
                 .HasForeignKey(r => r.FailureModeId);
 
+
+            //SIL Classification
+
             modelBuilder.Entity<SILClassification>().ToTable("tblSILClassification");
             modelBuilder.Entity<SILClassification>().HasKey(c => c.SILCId);
 
@@ -145,6 +148,8 @@ namespace Plant.DAL
                 .WithMany(p => p.calculation)
                 .HasForeignKey(p => p.SILCId);
 
+            //SIL Classification Master
+
             modelBuilder.Entity<SILClassificationMaster>().ToTable("tblSILClassificationMaster");
             modelBuilder.Entity<SILClassificationMaster>().HasKey(c => c.SILCMasterId);
 
@@ -155,18 +160,32 @@ namespace Plant.DAL
                 .WithMany(p => p.riskMatrixMaster)
                 .HasForeignKey(p => p.SILCMasterId);
 
+            modelBuilder.Entity<Category>().ToTable("tblCategory");
+            modelBuilder.Entity<Category>().HasKey(c => c.CategoryId);
+            modelBuilder.Entity<Category>()
+                .HasOne(p => p.riskMatrixMaster)
+                .WithMany(p => p.Category)
+                .HasForeignKey(p => p.RMMId);
+
+            modelBuilder.Entity<Severity>().ToTable("tblSeverity");
+            modelBuilder.Entity<Severity>().HasKey(c => c.SeverityId);
+            modelBuilder.Entity<Severity>()
+                .HasOne(p => p.riskMatrixMaster)
+                .WithMany(p => p.Severity)
+                .HasForeignKey(p => p.RMMId);
+
+            modelBuilder.Entity<TRF>().ToTable("tblTRF");
+            modelBuilder.Entity<TRF>().HasKey(c => c.TRFId);
+            modelBuilder.Entity<TRF>()
+                .HasOne(p => p.riskMatrixMaster)
+                .WithMany(p => p.TRF)
+                .HasForeignKey(p => p.RMMId);
+
             modelBuilder.Entity<InitiatingCausesMaster>().ToTable("tblInitiatingCausesMaster");
             modelBuilder.Entity<InitiatingCausesMaster>().HasKey(c => c.ICMId);
             modelBuilder.Entity<InitiatingCausesMaster>()
                 .HasOne(p => p.silClassificationMaster)
                 .WithMany(p => p.initiatingCausesMaster)
-                .HasForeignKey(p => p.SILCMasterId);
-
-            modelBuilder.Entity<ConditionalModifiersMaster>().ToTable("tblConditionalModifiersMaster");
-            modelBuilder.Entity<ConditionalModifiersMaster>().HasKey(c => c.CMMId);
-            modelBuilder.Entity<ConditionalModifiersMaster>()
-                .HasOne(p => p.silClassificationMaster)
-                .WithMany(p => p.conditionalModifiersMaster)
                 .HasForeignKey(p => p.SILCMasterId);
 
             modelBuilder.Entity<IPLMaster>().ToTable("tblIPLMaster");
@@ -175,6 +194,38 @@ namespace Plant.DAL
                 .HasOne(p => p.silClassificationMaster)
                 .WithMany(p => p.iplMaster)
                 .HasForeignKey(p => p.SILCMasterId);
+
+            //modelBuilder.Entity<ConditionalModifiersMaster>().ToTable("tblConditionalModifiersMaster");
+            //modelBuilder.Entity<ConditionalModifiersMaster>().HasKey(c => c.CMMId);
+            //modelBuilder.Entity<ConditionalModifiersMaster>()
+            //    .HasOne(p => p.silClassificationMaster)
+            //    .WithMany(p => p.conditionalModifiersMaster)
+            //    .HasForeignKey(p => p.SILCMasterId);
+
+            //modelBuilder.Entity<IP>().ToTable("tblIP");
+            //modelBuilder.Entity<IP>().HasKey(c => c.IPId);
+            //modelBuilder.Entity<IP>()
+            //    .HasOne(p => p.conditionalModifiersMaster)
+            //    .WithMany(p => p.IP)
+            //    .HasForeignKey(p => p.IPId);
+
+            //modelBuilder.Entity<PP>().ToTable("tblPP");
+            //modelBuilder.Entity<PP>().HasKey(c => c.PPId);
+            //modelBuilder.Entity<PP>()
+            //    .HasOne(p => p.conditionalModifiersMaster)
+            //    .WithMany(p => p.PP)
+            //    .HasForeignKey(p => p.PPId);
+
+            //modelBuilder.Entity<TR>().ToTable("tblTR");
+            //modelBuilder.Entity<TR>().HasKey(c => c.TRId);
+            //modelBuilder.Entity<TR>()
+            //    .HasOne(p => p.conditionalModifiersMaster)
+            //    .WithMany(p => p.TR)
+            //    .HasForeignKey(p => p.TRId);
+
+
+
+            //SIL Verification
 
             //modelBuilder.Entity<SIS>().ToTable("tblSIS");
             //modelBuilder.Entity<SIS>().HasKey(c => c.SISId);
@@ -213,8 +264,374 @@ namespace Plant.DAL
              .HasOne(p => p.sif)
              .WithMany(p => p.finalElement)
              .HasForeignKey(p => p.SIFId);
-        }
 
+
+
+            //Data Seeding
+            modelBuilder.Entity<SILClassificationMaster>()
+               .HasData(
+                new SILClassificationMaster
+                {
+                    SILCMasterId = 1
+                }
+               );
+
+            modelBuilder.Entity<RiskMatrixMaster>()
+              .HasData(
+               new RiskMatrixMaster
+               {
+                   SILCMasterId = 1,
+                   RMMId=1
+               }
+              );
+
+            modelBuilder.Entity<Category>()
+             .HasData(
+              new Category
+              {
+                  RMMId = 1,
+                  CategoryId=1,
+                  CategoryName="People"
+              },
+              new Category
+              {
+                  RMMId = 1,
+                  CategoryId = 2,
+                  CategoryName = "Environment"
+              },
+              new Category
+              {
+                  RMMId = 1,
+                  CategoryId = 3,
+                  CategoryName = "Asset"
+              }
+             );
+            modelBuilder.Entity<Severity>()
+             .HasData(
+              new Severity
+              {
+                  RMMId = 1,
+                  SeverityId=1,
+                  SeverityValue=1
+              },
+              new Severity
+              {
+                  RMMId = 1,
+                  SeverityId = 2,
+                  SeverityValue = 2
+              },
+              new Severity
+              {
+                  RMMId = 1,
+                  SeverityId = 3,
+                  SeverityValue = 3
+              },
+              new Severity
+              {
+                  RMMId = 1,
+                  SeverityId = 4,
+                  SeverityValue = 4
+              },
+              new Severity
+              {
+                  RMMId = 1,
+                  SeverityId = 5,
+                  SeverityValue = 5
+              },
+              new Severity
+              {
+                  RMMId = 1,
+                  SeverityId = 6,
+                  SeverityValue = 6
+              }
+             );
+            modelBuilder.Entity<TRF>()
+             .HasData(
+              new TRF
+              {
+                  RMMId = 1,
+                  TRFId = 1,
+                  TRFValue = 1.00E-01F
+              },
+              new TRF
+              {
+                  RMMId = 1,
+                  TRFId = 2,
+                  TRFValue = 1.00E-02F
+              },
+              new TRF
+              {
+                  RMMId = 1,
+                  TRFId = 3,
+                  TRFValue = 1.00E-03F
+              },
+              new TRF
+              {
+                  RMMId = 1,
+                  TRFId = 4,
+                  TRFValue = 1.00E-04F
+              },
+              new TRF
+              {
+                  RMMId = 1,
+                  TRFId = 5,
+                  TRFValue = 1.00E-05F
+              },
+              new TRF
+              {
+                  RMMId = 1,
+                  TRFId = 6,
+                  TRFValue = 1.00E-06F
+              }
+            );
+
+            modelBuilder.Entity<InitiatingCausesMaster>()
+             .HasData(
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 1,
+                  InitiatingCause= "Aboveground piping full breach failure (pipe size <= 150mm, 6 in)",
+                  IEF= 1.00E-06F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 2,
+                  InitiatingCause = "Aboveground piping full breach failure(pipe size > 150mm, 6 in)",
+                  IEF = 1.00E-07F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 3,
+                  InitiatingCause = "Aboveground piping leak (pipe size <=150mm, 6 in)",
+                  IEF = 1.00E-05F
+              },
+              new InitiatingCausesMaster
+              {
+                   SILCMasterId = 1,
+                   ICMId = 4,
+                   InitiatingCause = "Aboveground piping leak(pipe size > 150 mm, 6 in)",
+                   IEF = 1.00E-06F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 5,
+                  InitiatingCause = "Underground piping leak (full breach)",
+                  IEF = 1.00E-06F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 6,
+                  InitiatingCause = "Atmospheric tank catastrophic failure",
+                  IEF = 1.00E-05F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 7,
+                  InitiatingCause = "Atmspheric tank leak, 10-mm diameter, continuos",
+                  IEF = 1.00E-04F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 8,
+                  InitiatingCause = "BPCS control loop failure",
+                  IEF = 1.00E-01F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 9,
+                  InitiatingCause = "Cooling water failure",
+                  IEF = 1.00E-01F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 10,
+                  InitiatingCause = "Crane load drop",
+                  IEF = 1.00E-03F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 11,
+                  InitiatingCause = "External fire, large (aggregate causes)",
+                  IEF = 1.00E-02F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 12,
+                  InitiatingCause = "External fire, small (aggregate causes)",
+                  IEF = 1.00E-01F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 13,
+                  InitiatingCause = "External Impact (by backbone, vehicle etc) ",
+                  IEF = 1.00E-02F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 14,
+                  InitiatingCause = "Future of check valve (hgh demand)",
+                  IEF = 1.00E-01F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 15,
+                  InitiatingCause = "Failure of double check valves in series (high demand)",
+                  IEF = 1.00E-02F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 16,
+                  InitiatingCause = "Hose leak",
+                  IEF = 1.00E-01F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 17,
+                  InitiatingCause = "Hose rupture",
+                  IEF = 1.00E-02F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 18,
+                  InitiatingCause = "Human error during task performed less than once per month",
+                  IEF = 1.00E-02F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 19,
+                  InitiatingCause = "Human error during task performed more than once per month but less than once per week",
+                  IEF = 1.00E-01F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 20,
+                  InitiatingCause = "Human error during task performed once per week or more ofter",
+                  IEF = 1.00E+00F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 21,
+                  InitiatingCause = "Lightning strike",
+                  IEF = 1.00E-03F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 22,
+                  InitiatingCause = "Operator failure (to execute a complete, routine procedure; well-trained operator,unstressed, not fatigued)",
+                  IEF = 1.00E-02F
+              }, new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 23,
+                  InitiatingCause = "Premature opening of spring loaded relief valve",
+                  IEF = 1.00E-02F
+              }, new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 24,
+                  InitiatingCause = "Pressure regulator failure",
+                  IEF = 1.00E-01F
+              },
+              new InitiatingCausesMaster
+              {
+                  SILCMasterId = 1,
+                  ICMId = 25,
+                  InitiatingCause = "Pressure vesssel catastrophic failure",
+                  IEF = 1.00E-05F
+              },
+              new InitiatingCausesMaster
+               {
+                   SILCMasterId = 1,
+                   ICMId = 26,
+                   InitiatingCause = "Pump seal complete failure",
+                   IEF = 1.00E-01F
+               },
+               new InitiatingCausesMaster
+               {
+                    SILCMasterId = 1,
+                    ICMId = 27,
+                    InitiatingCause = "Pump seal leak",
+                    IEF = 1.00E+00F
+               },
+               new InitiatingCausesMaster
+               {
+                     SILCMasterId = 1,
+                     ICMId = 28,
+                     InitiatingCause = "Pump, compressor, fan or blower failure",
+                     IEF = 1.00E-01F
+               },
+               new InitiatingCausesMaster
+               {
+                     SILCMasterId = 1,
+                     ICMId = 29,
+                     InitiatingCause = "Screw conveyor failure",
+                     IEF = 1.00E+00F
+               },
+               new InitiatingCausesMaster
+               {
+                   SILCMasterId = 1,
+                   ICMId = 30,
+                   InitiatingCause = "Screw conveyor overheating of material",
+                   IEF = 1.00E-01F
+               },
+               new InitiatingCausesMaster
+               {
+                   SILCMasterId = 1,
+                   ICMId = 31,
+                   InitiatingCause = "Single circuit loss of power",
+                   IEF = 1.00E-01F
+               },
+               new InitiatingCausesMaster
+               {
+                   SILCMasterId = 1,
+                   ICMId = 32,
+                   InitiatingCause = "Spurious operation of safety controls, Alarms  & interlocks",
+                   IEF = 1.00E-01F
+               },
+               new InitiatingCausesMaster
+               {
+                   SILCMasterId = 1,
+                   ICMId = 33,
+                   InitiatingCause = "Turbine/diesel engine overspeed with casing breach",
+                   IEF = 1.00E-04F
+               },
+               new InitiatingCausesMaster
+               {
+                   SILCMasterId = 1,
+                   ICMId = 34,
+                   InitiatingCause = "Malfunction of Level Transmitter",
+                   IEF = 1.00E-03F
+               }
+             );
+
+
+            
+
+           
+        }
     }
+
 }
+
 
--- Plant/Plant/Models/SILClassificationMaster.cs
+++ Plant/Plant/Models/SILClassificationMaster.cs
@@ -7,21 +7,42 @@ namespace Plant.Models
     public class SILClassificationMaster
     {
         public int SILCMasterId { get; set; }
-        public string MachineId { get; set; }
-        public string ImpactEvent { get; set; }
+        //public string MachineId { get; set; }
+        //public string ImpactEvent { get; set; }
         public List<RiskMatrixMaster> riskMatrixMaster { get; set; }
         public List<InitiatingCausesMaster> initiatingCausesMaster { get; set; }
-        public List<ConditionalModifiersMaster> conditionalModifiersMaster { get; set; }
+        //public List<ConditionalModifiersMaster> conditionalModifiersMaster { get; set; }
         public List<IPLMaster> iplMaster { get; set; }
     }
     public class RiskMatrixMaster
     {
         public int RMMId { get; set; }
         public int SILCMasterId { get; set; }
-        public string Category { get; set; }
-        public int Severity { get; set; }
-        public float TRF { get; set; }   //Tolerable Risk Frequency or TMEL
+        public List<Category> Category { get; set; }
+        public List<Severity> Severity { get; set; }
+        public List<TRF> TRF { get; set; }   //Tolerable Risk Frequency or TMEL
         public SILClassificationMaster silClassificationMaster { get; set; }
+    }
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public int RMMId { get; set; }
+        public string CategoryName { get; set; }
+        public RiskMatrixMaster riskMatrixMaster { get; set; }
+    }
+    public class Severity
+    {
+        public int SeverityId { get; set; }
+        public int RMMId { get; set; }
+        public int SeverityValue { get; set; }
+        public RiskMatrixMaster riskMatrixMaster { get; set; }
+    }
+    public class TRF
+    {
+        public int TRFId { get; set; }
+        public int RMMId { get; set; }
+        public float TRFValue  { get; set; }
+        public RiskMatrixMaster riskMatrixMaster { get; set; }
     }
     public class InitiatingCausesMaster
     {
@@ -29,15 +50,6 @@ namespace Plant.Models
         public int SILCMasterId { get; set; }
         public string InitiatingCause { get; set; }
         public float IEF { get; set; }   //Initiating Event Frequency
-        public SILClassificationMaster silClassificationMaster { get; set; }
-    }
-    public class ConditionalModifiersMaster
-    {
-        public int CMMId { get; set; }
-        public int SILCMasterId { get; set; }
-        public float IP { get; set; }      //Ignition Probability
-        public float PP { get; set; }     //People Present
-        public float TR { get; set; }   //Times at Risk
         public SILClassificationMaster silClassificationMaster { get; set; }
     }
     public class IPLMaster  //Independent Protection Layer
@@ -48,4 +60,35 @@ namespace Plant.Models
         public float PFD { get; set; }   //Probability of Failure on Demand
         public SILClassificationMaster silClassificationMaster { get; set; }
     }
+    //public class ConditionalModifiersMaster
+    //{
+    //    public int CMMId { get; set; }
+    //    public int SILCMasterId { get; set; }
+    //    public List<IP> IP { get; set; }      //Ignition Probability
+    //    public List<PP> PP { get; set; }     //People Present
+    //    public List<TR> TR { get; set; }   //Times at Risk
+    //    public SILClassificationMaster silClassificationMaster { get; set; }
+    //}
+    //public class IP
+    //{
+    //    public int IPId { get; set; }
+    //    public int CMMId { get; set; }
+    //    public float IPValue { get; set; }
+    //    public ConditionalModifiersMaster conditionalModifiersMaster { get; set; }
+    //}
+    //public class PP
+    //{
+    //    public int PPId { get; set; }
+    //    public int CMMId { get; set; }
+    //    public float PPValue { get; set; }
+    //    public ConditionalModifiersMaster conditionalModifiersMaster { get; set; }
+    //}
+    //public class TR
+    //{
+    //    public int TRId { get; set; }
+    //    public int CMMId { get; set; }
+    //    public float TRValue { get; set; }
+    //    public ConditionalModifiersMaster conditionalModifiersMaster { get; set; }
+    //}
+   
 }
